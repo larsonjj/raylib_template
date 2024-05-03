f32486b5e22d2aeb28baae63a2f692b193a2bf4b
Jake Larson <jake.j.larson@gmail.com>
2024-05-02 19:56:55 -0500

    build: Update Raylib submodule to use OpenGL version 3.3 since ES 2.0 + angle doesn't work with VSync

--- src/main.c
+++ src/main.c
@@ -36,40 +36,27 @@ void DrawEntity(const Entity *entity, Conversion cv)
 
     // Have to negate rotation to account for y-flip
     DrawTextureEx(entity->texture, ps, -RAD2DEG * radians, textureScale, WHITE);
-
-    // I used these circles to ensure the coordinate transformation was correct
-    DrawCircleV(ps, 5.0f, BLACK);
-    p = b2Body_GetWorldPoint(entity->bodyId, (b2Vec2){0.0f, 0.0f});
-    ps = ConvertWorldToScreen(p, cv);
-    DrawCircleV(ps, 5.0f, BLUE);
-    p = b2Body_GetWorldPoint(entity->bodyId, (b2Vec2){0.5f * cv.tileSize, -0.5f * cv.tileSize});
-    ps = ConvertWorldToScreen(p, cv);
-    DrawCircleV(ps, 5.0f, RED);
 }
 
 int main(void)
 {
-    // Initialization
-    //--------------------------------------------------------------------------------------
     const int screenWidth = 1280;
     const int screenHeight = 720;
 
-    SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI | FLAG_VSYNC_HINT);
+    SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
     InitWindow(screenWidth, screenHeight, "raylib box2d - custom frame control");
 
-    // Custom timming variables
-    double previousTime = GetTime(); // Previous time measure
-    double currentTime = 0.0;        // Current time measure
-    double updateDrawTime = 0.0;     // Update + Draw time
-    double waitTime = 0.0;           // Wait time (if target fps required)
-    float deltaTime = 0.0f;          // Frame time (Update + Draw + Wait time)
+    bool pause = false; // Pause control flag
+    int targetFPS = 60;
 
-    float timeCounter = 0.0f; // Accumulative time counter (seconds)
-    float position = 0.0f;    // Circle position
-    bool pause = false;       // Pause control flag
+    double timeCounter = 0.0;
+    float fixedFPS = 60.0f;
+    double fixedTimeStep = 1.0f / fixedFPS;
 
-    int targetFPS = 60; // Our initial target fps
-    float targetDeltaTime = 1.0f / (float)targetFPS;
+    double currentTime = GetTime();
+    double accumulator = 0.0;
+
+    float position = 0.0f; // Circle position
 
     float tileSize = 1.0f;
     float scale = 50.0f;
@@ -93,7 +80,7 @@ int main(void)
         bodyDef.position = (b2Vec2){(1.0f * i - 10.0f) * tileSize, -4.5f - 0.5f * tileSize};
 
         // I used this rotation to test the world to screen transformation
-        //bodyDef.angle = 0.25f * b2_pi * i;
+        bodyDef.angle = 0.25f * b2_pi * i;
 
         entity->bodyId = b2CreateBody(worldId, &bodyDef);
         entity->texture = textures[0];
@@ -114,46 +101,63 @@ int main(void)
         shapeDef.restitution = 0.1f;
         b2CreatePolygonShape(entity->bodyId, &shapeDef, &tilePolygon);
     }
-    //--------------------------------------------------------------------------------------
 
-    // Main game loop
-    while (!WindowShouldClose()) // Detect window close button or ESC key
+    while (!WindowShouldClose())
     {
-        // if (updateDrawTime > targetDeltaTime)
-        // {
-
         // Update
+        //----------------------------------------------------------------------------------
+
+        double newTime = GetTime();
+        double frameTime = newTime - currentTime;
+
+        // Avoid spiral of death if CPU  can't keep up with target FPS
+        if (frameTime > 0.25)
+        {
+            frameTime = 0.25;
+        }
+
+        currentTime = newTime;
+
+
+        // Input
         //----------------------------------------------------------------------------------
         PollInputEvents(); // Poll input events (SUPPORT_CUSTOM_FRAME_CONTROL)
 
         if (IsKeyPressed(KEY_SPACE))
+        {
             pause = !pause;
-
-        if (IsKeyPressed(KEY_UP))
-            targetFPS += 1000;
-        else if (IsKeyPressed(KEY_DOWN))
-            targetFPS -= 1000;
-
-        if (targetFPS < 0)
-            targetFPS = 0;
+        }
 
         if (!pause)
         {
-            position += 200 * deltaTime; // We move at 200 pixels per second
-            if (position >= (float)GetScreenWidth())
-                position = 0;
-            timeCounter += deltaTime; // We count time (seconds)
-            b2World_Step(worldId, deltaTime, 4);
-            timeCounter += deltaTime; // We count time (seconds)
+            accumulator += frameTime;
+
+            while (accumulator >= fixedTimeStep)
+            {
+                // Fixed Update
+                //--------------------------------------------------------------------------
+
+                position += (float)(200 * fixedTimeStep); // We move at 200 pixels per second
+                if (position >= (float)GetScreenWidth())
+                    position = 0;
+                timeCounter += fixedTimeStep; // We count time (seconds)
+                b2World_Step(worldId, (float)fixedTimeStep, 4);
+
+                timeCounter += fixedTimeStep;
+                accumulator -= fixedTimeStep;
+            }
+
+            // Left over time to be used for interpolation within Update
+            const double alpha = accumulator / fixedTimeStep;
         }
+
+        // Drawing
         //----------------------------------------------------------------------------------
 
-        // Draw
-        //----------------------------------------------------------------------------------
         BeginDrawing();
 
-        ClearBackground(RAYWHITE);
-
+        ClearBackground(GRAY);
+        // Draw
         for (int i = 0; i < 20; ++i)
         {
             DrawEntity(groundEntities + i, cv);
@@ -164,83 +168,27 @@ int main(void)
             DrawEntity(boxEntities + i, cv);
         }
 
-        const char *message = "Hello Box2D!";
-        int fontSize = 36;
-        int textWidth = MeasureText("Hello Box2D!", fontSize);
-        DrawText(TextFormat("timeCounter: %03.0f ms", timeCounter * 1000.0f),
-                 40,
-                 GetScreenHeight() / 2 - 100,
-                 20,
-                 MAROON);
-        DrawText(TextFormat("deltaTime: %f ms", deltaTime),
-                 40,
-                 GetScreenHeight() / 2 - 50,
-                 20,
-                 MAROON);
-        DrawText(TextFormat("updateDrawTime: %f ms", updateDrawTime),
-                 40,
-                 GetScreenHeight() / 2 - 0,
-                 20,
-                 MAROON);
-        DrawText(message, (screenWidth - textWidth) / 2, 50, fontSize, LIGHTGRAY);
-
-
-        for (int i = 0; i < GetScreenWidth() / 200; i++)
-            DrawRectangle(200 * i, 0, 1, GetScreenHeight(), SKYBLUE);
-
-        DrawCircle((int)position, GetScreenHeight() / 2 - 25, 50, RED);
-
-        DrawText("Circle is moving at a constant 200 pixels/sec,\nindependently of the frame rate.",
-                 10,
-                 10,
-                 20,
-                 DARKGRAY);
-        DrawText("PRESS SPACE to PAUSE MOVEMENT", 10, GetScreenHeight() - 60, 20, GRAY);
-        DrawText("PRESS UP | DOWN to CHANGE TARGET FPS", 10, GetScreenHeight() - 30, 20, GRAY);
-        DrawText(TextFormat("TARGET FPS: %i", targetFPS), GetScreenWidth() - 220, 10, 20, LIME);
-        DrawText(TextFormat("CURRENT FPS: %i", (int)(1.0f / deltaTime)),
-                 GetScreenWidth() - 220,
+        DrawCircle((int)position, GetScreenHeight() / 2, 50, RED);
+        DrawText("PRESS SPACE to PAUSE MOVEMENT", 20, GetScreenHeight() - 40, 20, WHITE);
+        DrawText(TextFormat("Fixed FPS: %i", (int)fixedFPS), GetScreenWidth() - 200, 20, 20, GREEN);
+        DrawText(TextFormat("Current FPS: %i", (int)(1.0f / frameTime)),
+                 GetScreenWidth() - 200,
                  40,
                  20,
                  GREEN);
 
         EndDrawing();
 
-        // NOTE: In case raylib is configured to SUPPORT_CUSTOM_FRAME_CONTROL,
-        // Events polling, screen buffer swap and frame time control must be managed by the user
-
         SwapScreenBuffer(); // Flip the back buffer to screen (front buffer)
-        // }
-
-        currentTime = GetTime();
-        updateDrawTime = currentTime - previousTime;
-
-        // if (targetFPS > 0) // We want a fixed frame rate
-        // {
-        //     waitTime = targetDeltaTime - updateDrawTime;
-        //     if (waitTime > 0.0)
-        //     {
-        //         WaitTime((float)waitTime * 0.95f);
-        //         currentTime = GetTime();
-        //         deltaTime = (float)(currentTime - previousTime);
-        //     }
-        // }
-        // else
-        // {
-        deltaTime = (float)updateDrawTime; // Framerate could be variable
-        // }
-
-        previousTime = currentTime;
-        //----------------------------------------------------------------------------------
     }
 
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
+    // Cleanup
+    //-------------------------------------------------------------------------------------
+
     UnloadTexture(textures[0]);
     UnloadTexture(textures[1]);
 
-    CloseWindow(); // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
+    CloseWindow();
 
     return 0;
 }
